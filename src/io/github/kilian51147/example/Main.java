896df0b44ca6b9ed4b7a0131ea5d183d2858c948
Kilian51147 <83707468+Kilian51147@users.noreply.github.com>
2021-07-13 03:50:13 +0200

    Add files via upload

---
+++ src/io/github/kilian51147/example/Main.java
@@ -0,0 +1,19 @@
+package io.github.kilian51147.example;
+
+import io.github.kilian51147.Language;
+import io.github.kilian51147.TranslationConfiguration;
+
+public class Main 
+{
+	private static TranslationConfiguration config = new TranslationConfiguration();
+	
+	public static void main(String[] args)
+	{
+		config.setLanguage(Language.SPANISH);
+		
+		System.out.println(config.get("test.one"));
+		System.out.println(config.get("test.two"));
+		System.out.println(config.get("test.three"));
+		System.out.println(config.get("test.four"));
+	}
+}
